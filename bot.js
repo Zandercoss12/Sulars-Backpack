a1209e7bff9dddac620b5456d16176ee1ba1362d
Zandercoss12 <tuoi.nguyen69@icloud.com>
2017-09-28 11:52:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,18 @@
 const Discord = require('discord.js');
 var prefix = "-+";
 const client = new Discord.Client();
-
+var fortunes = [
+  "**ZanderBot says**\n```css\nYes```",
+  "**ZanderBot says**\n```fix\nNo```",
+  "**ZanderBot says**\n```fix\nhm? Did you say something?```",
+  "**ZanderBot says**\n```css\nYes```",
+  "**ZanderBot says**\n```fix\nNo```",
+  "**ZanderBot says**\n```fix\nU dumb```",
+  "**ZanderBot says**\n```css\nMaybe so...```",
+  "**ZanderBot says**\n```fix\nI do not think so.```",
+  "**ZanderBot says**\n```css\nThe world may never know...```",
+  "**ZanderBot says**\n```fix\nNever in a million years.```"
+];
 
 
 client.login(process.env.BOT_TOKEN);
