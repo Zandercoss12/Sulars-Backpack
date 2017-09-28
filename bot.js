2772e547fe4c13410f553a339b70be85e51180f5
Zandercoss12 <tuoi.nguyen69@icloud.com>
2017-09-28 18:30:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,11 +96,12 @@ client.on("message", function(message) {
 	if(message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	var args = message.content.substring(prefix.length).split(" ");
 
 	switch (args[0].toLowerCase()) {
 		case "say":
-		const sayMessage = args.join(" ");
+		const argz = message.content.slice(prefix.length).trim().split(/ +/g);
+		const sayMessage = argz.join(" ");
    		 message.delete().catch(O_o=>{});
    		 message.channel.send(sayMessage);
    		 break;
