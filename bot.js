16dad05f597ee56ec73cf8074207c20019120feb
Zandercoss12 <tuoi.nguyen69@icloud.com>
2017-09-28 19:34:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,4 +139,10 @@ client.on("message", async message => {
   }
 });
 
+setInterval(() => {
+ http.get('http://discordjs-heroku.herokuapp.com');
+}, 900000);
+
+const port = process.env.PORT || 5000;
+
 client.login(process.env.BOT_TOKEN);
