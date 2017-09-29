1eee682057875a32fb7f81e640727483b763f084
Zandercoss12 <tuoi.nguyen69@icloud.com>
2017-09-29 11:00:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -141,7 +141,7 @@ client.on("message", async message => {
     message.channel.sendMessage(facts[Math.floor(Math.random() * facts.length)]);
   }
   
-if(command === "Where's-my-super-suit") {
+  if(command === "Supersuit") {
     message.channel.sendMessage(supersuit[Math.floor(Math.random() * supersuit.length)]);
   }
   
