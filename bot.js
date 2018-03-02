1fc92889ba64ae26f94a835bb246ee851c5ce502
Zandercross12 <tuoi.nguyen69@icloud.com>
2018-03-02 11:12:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ client.on("message", async message => {
 
   if(command === "info") {
     var embed = new Discord.RichEmbed()
-      .setThumbnail(client.avatarURL)
+      .setThumbnail(<@361944021175631872>.avatarURL)
       .addField("What I am", "I am a bot created by: Zandercros12!", true)
       .addField("What I do", "Random Stuff? I guess.")
       .addField("Am I a good bot?", "no")
