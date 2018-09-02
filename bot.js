7c27801cc5481ed31d3a1ad6533dd7cf8e0b6f0c
Zandercross12 <tuoi.nguyen69@icloud.com>
2018-09-02 09:38:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ var facts = [
 ];
   
 client.on('ready', () => {
-  client.user.setPresence({game: {name: "-+help for commands!", type: "STREAMING"}});
+  client.user.setPresence({game: {name: "-+help for commands!", type: "streaming"}});
   console.log('I am ready!');
 });
 
